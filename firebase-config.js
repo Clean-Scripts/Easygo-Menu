22703f4948e9a52d1d21f5559d6dce66d075be3f
ꪀ꠸᥉ꫝᥲꪀꪻ  ꠹ᥲ꠸᥉ᥕᥲᥣ <248268862+Clean-Scripts@users.noreply.github.com>
2026-01-04 22:01:37 +0530

    Update and rename firebase-orders.js to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBkBWtdxiN92TOqQni5lM_w1YQfydsKwG0",
+  authDomain: "easygoooo.firebaseapp.com",
+  databaseURL: "https://easygoooo-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "easygoooo",
+  storageBucket: "easygoooo.firebasestorage.app",
+  messagingSenderId: "895871235594",
+  appId: "1:895871235594:web:e918f85a8c4da2ecd391f3",
+  measurementId: "G-VPBJ2QKN1T"
+};
